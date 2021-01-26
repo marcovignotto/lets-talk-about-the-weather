5b37f689ba91975e8c9fd81969fe8311783b7277
Marco Vignotto <marcovignotto81@gmail.com>
2021-01-26 15:54:33 +0100

    make old blocks ok

--- js/weatherBlock.js
+++ js/weatherBlock.js
@@ -1,58 +1,53 @@
 // WEATHER
-getWeather("berlin", "en", "metric").then((result) => {
-  const res = result;
-  console.log(res);
+const makeWeatherBlock = (loc, lang, unit) => {
+  getWeather(loc, lang, unit).then((result) => {
+    const res = result;
+    console.log(res);
 
-  // get data
-  let getLocation = res.name;
-  let getMain = res.weather[0].main;
-  let getMainDesc = res.weather[0].description;
-  let getTemp = res.main.temp;
-  let getWind = res.wind.speed;
+    // get data
+    let getLocation = res.name;
+    let getMain = res.weather[0].main;
+    let getMainDesc = res.weather[0].description;
+    let getTemp = res.main.temp;
+    let getWind = res.wind.speed;
 
-  // get container
-  const elementContainer = document.querySelector(
-    ".container__weather__blocks"
-  );
+    // get container
+    const elementContainer = document.querySelector(
+      ".container__weather__blocks"
+    );
 
-  // create block container
-  const blockWeather = document.createElement("div");
-  blockWeather.className = "block__weather";
+    // create block container
+    const blockWeather = document.createElement("div");
+    blockWeather.className = "block__weather";
 
-  const location = document.createElement("h4");
-  location.className = "block__weather__location";
-  location.innerHTML = `${getLocation}`;
+    const location = document.createElement("h4");
+    location.className = "block__weather__location";
+    location.innerHTML = `${getLocation}`;
 
-  const main = document.createElement("h4");
-  main.className = "block__weather__main";
-  main.innerHTML = `${getMain}`;
+    const main = document.createElement("h4");
+    main.className = "block__weather__main";
+    main.innerHTML = `${getMain}`;
 
-  const mainDesc = document.createElement("h4");
-  mainDesc.className = "block__weather__main";
-  mainDesc.innerHTML = `${getMainDesc}`;
+    const mainDesc = document.createElement("h4");
+    mainDesc.className = "block__weather__main";
+    mainDesc.innerHTML = `${getMainDesc}`;
 
-  const temp = document.createElement("h4");
-  temp.className = "block__weather__temp";
-  temp.innerHTML = `${getTemp}`;
+    const temp = document.createElement("h4");
+    temp.className = "block__weather__temp";
+    temp.innerHTML = `${getTemp}`;
 
-  const wind = document.createElement("h4");
-  wind.className = "block__weather__wind";
-  wind.innerHTML = `${getWind}`;
+    const wind = document.createElement("h4");
+    wind.className = "block__weather__wind";
+    wind.innerHTML = `${getWind}`;
 
-  // merge all
-  blockWeather.innerHTML +=
-    location.outerHTML +
-    main.outerHTML +
-    mainDesc.outerHTML +
-    temp.outerHTML +
-    wind.outerHTML;
+    // merge all
+    blockWeather.innerHTML +=
+      location.outerHTML +
+      main.outerHTML +
+      mainDesc.outerHTML +
+      temp.outerHTML +
+      wind.outerHTML;
 
-  elementContainer.appendChild(blockWeather);
-});
-
-// listItem.innerHTML +=
-//   listItemCheckbox.outerHTML +
-//   listItemLabel.outerHTML +
-//   editButton.outerHTML +
-//   deleteButton.outerHTML;
-// listElement.appendChild(listItem);
+    elementContainer.appendChild(blockWeather);
+  });
+};
