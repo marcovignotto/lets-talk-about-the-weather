6701c7c3b8dce9d00ec19e24d9299ad222d7a033
Marco Vignotto <marcovignotto81@gmail.com>
2021-02-02 17:11:10 +0100

    bearer auth ok

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,16 @@ const config = require("config");
 
 module.exports = function (req, res, next) {
   // get token
-  const token = req.header("x-auth-token") || req.header("Bearer");
-  // const token = req.header("Bearer");
-  // check if not token
+  // const token = req.header("x-auth-token");
+
+  // for bearer token auth
+  const token = req.header("Authorization").split(" ")[1];
+
   if (!token) res.status(401).json({ msg: "No token, no auth!" });
 
   try {
     const decoded = jwt.verify(token, config.get("authLocalApi.jwtSecret"));
+
     req.user = decoded.user;
     next();
   } catch (err) {
