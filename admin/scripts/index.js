275549f82505fa3291759d140da0aac392e8a43a
Marco Vignotto <marcovignotto81@gmail.com>
2021-02-04 17:13:10 +0100

    update row and new row added

--- admin/scripts/index.js
+++ admin/scripts/index.js
@@ -121,102 +121,89 @@ if (!sessionStorage.getItem("isAuthenticated")) {
 }
 
 const inputs = {
-  // e: this.e,
-  // number: this.e.target,
-  // myTarget: this.number,
   id: function (e) {
-    return e.target.getAttribute("data-id");
+    return e.target.parentElement.parentElement.getAttribute("data-id");
   },
-  menu: function (e) {
-    return e.target.parentElement.closest(".menu");
+  gridItem: function (e) {
+    return e.target.parentElement.closest(".grid__item");
+  },
+  menuRow: function (e) {
+    return e.target.parentElement.parentElement.querySelector(".menu");
+  },
+  userToEdit: function (id = "") {
+    let userToEdit = arrAllUsers.find((x) => x._id === id);
+    return userToEdit;
   },
 
-  div: function (e) {
+  row: function (
+    _id = "",
+    firstName = "",
+    location = "",
+    language = "",
+    unit = ""
+  ) {
     const row = document.createElement("div");
     row.className = `row grid__item__edit pb-1`;
-    row.style.backgroundColor = "red";
-    row.innerHTML = this.id(e);
-    this.menu(e).appendChild(row);
+
+    const firstNameCol = document.createElement("input");
+    firstNameCol.className = "col-3 edit__input first__name";
+    firstNameCol.setAttribute("type", "text");
+    firstNameCol.setAttribute("value", firstName);
+
+    const locationCol = document.createElement("input");
+    locationCol.className = "col-4 edit__input location";
+    locationCol.setAttribute("type", "text");
+    locationCol.setAttribute("value", location);
+
+    const languageCol = document.createElement("input");
+    languageCol.className = "col-1 edit__input language";
+    languageCol.setAttribute("type", "text");
+    languageCol.setAttribute("value", language);
+
+    const unitCol = document.createElement("input");
+    unitCol.className = "col-2 edit__input unit";
+    unitCol.setAttribute("type", "text");
+    unitCol.setAttribute("value", unit);
+
+    const editIcon = document.createElement("div");
+    editIcon.className = "col-1 save__icon";
+    editIcon.dataset.id = _id;
+    editIcon.innerHTML = `<button class="btn__update__weather__user"><i class="fas fa-save"></i></button>`;
+
+    const deleteIcon = document.createElement("div");
+    deleteIcon.className = "col-1 undo__icon";
+    deleteIcon.dataset.id = _id;
+    deleteIcon.innerHTML = `<button class="btn__undo__weather__user"><i class="fas fa-times"></i></button>`;
+
+    row.innerHTML +=
+      firstNameCol.outerHTML +
+      locationCol.outerHTML +
+      languageCol.outerHTML +
+      unitCol.outerHTML +
+      editIcon.outerHTML +
+      deleteIcon.outerHTML;
+
+    return row;
   },
 };
 
-btnAddUser.addEventListener("click", function (e) {
-  console.log(e.target.getAttribute("data-id"));
-  inputs.div(e);
-});
-
 // EDIT & DELETE
 const submitEdit = (e) => {
-  // get id from event
-  let id = e.target.parentElement.parentElement.getAttribute("data-id");
-  // get grdItem
-  let gridItem = e.target.parentElement.closest(".grid__item");
-  // extract obj
-  let userToEdit = arrAllUsers.find((x) => x._id === id);
+  const { _id, firstName, location, language, unit } = inputs.userToEdit(
+    inputs.id(e)
+  );
 
-  // destructuring
-  const { firstName, location, language, unit } = userToEdit;
-
-  const row = document.createElement("div");
-  row.className = `row grid__item__edit pb-1`;
-
-  const firstNameCol = document.createElement("input");
-  firstNameCol.className = "col-3 edit__input first__name";
-  firstNameCol.setAttribute("type", "text");
-  firstNameCol.setAttribute("value", firstName);
-
-  const locationCol = document.createElement("input");
-  locationCol.className = "col-4 edit__input location";
-  locationCol.setAttribute("type", "text");
-  locationCol.setAttribute("value", location);
-
-  const languageCol = document.createElement("input");
-  languageCol.className = "col-1 edit__input language";
-  languageCol.setAttribute("type", "text");
-  languageCol.setAttribute("value", language);
-
-  const unitCol = document.createElement("input");
-  unitCol.className = "col-2 edit__input unit";
-  unitCol.setAttribute("type", "text");
-  unitCol.setAttribute("value", unit);
-
-  const editIcon = document.createElement("div");
-  editIcon.className = "col-1 save__icon";
-  editIcon.dataset.id = id;
-  editIcon.innerHTML = `<button class="btn__update__weather__user"><i class="fas fa-save"></i></button>`;
-
-  const deleteIcon = document.createElement("div");
-  deleteIcon.className = "col-1 undo__icon";
-  deleteIcon.dataset.id = id;
-  deleteIcon.innerHTML = `<button class="btn__undo__weather__user"><i class="fas fa-times"></i></button>`;
-
-  row.innerHTML +=
-    firstNameCol.outerHTML +
-    locationCol.outerHTML +
-    languageCol.outerHTML +
-    unitCol.outerHTML +
-    editIcon.outerHTML +
-    deleteIcon.outerHTML;
-
-  gridItem.after(row);
-
-  let weatherUserObj = {
-    _id: id,
-    firstName: firstNameCol.value,
-    location,
-    language,
-    unit,
-  };
-
-  // iconsEditInit(id, firstName, location, language, unit);
-  // console.log(firstNameCol.value);
-  // console.log(weatherUserObj);
-
-  // function getUpdates() {
-  //   console.log(firstNameCol.value);
-  // }
+  inputs
+    .gridItem(e)
+    .after(inputs.row(_id, firstName, location, language, unit));
 
   iconsEditInit();
+};
+
+const addNewUserInputs = (e) => {
+  inputs.menuRow(e).after(inputs.row());
+
+  // iconsEditInit();
 };
 
 const submitDelete = async (e) => {
@@ -336,6 +323,10 @@ const sendLogin = async (e) => {
 // BTNs
 btnLogin.addEventListener("click", sendLogin);
 
+btnAddUser.addEventListener("click", function (e) {
+  console.log(e.target.parentElement.parentElement.querySelector(".menu"));
+  addNewUserInputs(e);
+});
 // ICONS
 const iconsInit = () => {
   document.querySelectorAll(".edit__icon").forEach((x) =>
