bdb716e3a3759e1e39dfdfe26155adf10bdee7db
Marco Vignotto <marcovignotto81@gmail.com>
2021-02-23 10:18:39 +0100

    delete user location after weather user ok

--- routes/locations.js
+++ routes/locations.js
@@ -130,7 +130,7 @@ router.put("/:id", auth, async (req, res) => {
 
 // @route   DELETE api/location/:id
 // @desc    Delete location
-// @access  Private
+// @access  Private rivate
 
 router.delete("/:id", auth, async (req, res) => {
   try {
