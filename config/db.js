52b85e6f95625add9f71f88cd62206920f3a0c20
Marco Vignotto <marcovignotto81@gmail.com>
2021-03-01 09:00:50 +0100

    added process mongoUri, removed grunt

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoDb.URI");
+const db = process.env.MONGODB_URI;
 
 const dbOptions = {
   useNewUrlParser: true,
