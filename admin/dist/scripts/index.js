ed593ea36e4c0e34745f932b00cdae6a84508bbb
Marco Vignotto <marcovignotto81@gmail.com>
2021-05-10 13:38:35 +0200

    Added process.env in ServerCtrl

--- admin/dist/scripts/index.js
+++ admin/dist/scripts/index.js
@@ -525,17 +525,62 @@ const ItemCtrl = (function () {
 })();
 
 const ServerCtrl = (function () {
+  const devUrl = "http://localhost:5000";
+  const prodUrl = "lets-talk-about-the-weather.herokuapp.com";
+
   const URLs = {
-    URL_POST: "http://localhost:5000/api/auth",
-    URL_POST_USER: "http://127.0.0.1:5000/api/weatherusers/",
-    URL_GET: "http://127.0.0.1:5000/api/weatherusers",
-    URL_DELETE: "http://127.0.0.1:5000/api/weatherusers/",
-    URL_PUT: "http://127.0.0.1:5000/api/weatherusers/",
-    URL_GET_LOCATION: "http://127.0.0.1:5000/api/locations/",
-    URL_POST_LOCATION: "http://127.0.0.1:5000/api/locations/",
-    URL_PUT_LOCATION: "http://127.0.0.1:5000/api/locations/",
-    URL_DELETE_LOCATION: "http://127.0.0.1:5000/api/locations/",
+    URL_POST:
+      process.env.NODE_ENV === "production" ? prodUrl : devUrl + "/api/auth",
+    URL_POST_USER:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/weatherusers/",
+    URL_GET:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/weatherusers",
+    URL_DELETE:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/weatherusers/",
+    URL_PUT:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/weatherusers/",
+    URL_GET_LOCATION:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/locations/",
+    URL_POST_LOCATION:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/locations/",
+    URL_PUT_LOCATION:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/locations/",
+    URL_DELETE_LOCATION:
+      process.env.NODE_ENV === "production"
+        ? prodUrl
+        : devUrl + "/api/locations/",
   };
+
+  // const URLsProd = {
+  //   URL_POST: "http://localhost:5000/api/auth",
+  //   URL_POST_USER: "http://127.0.0.1:5000/api/weatherusers/",
+  //   URL_GET: "http://127.0.0.1:5000/api/weatherusers",
+  //   URL_DELETE: "http://127.0.0.1:5000/api/weatherusers/",
+  //   URL_PUT: "http://127.0.0.1:5000/api/weatherusers/",
+  //   URL_GET_LOCATION: "http://127.0.0.1:5000/api/locations/",
+  //   URL_POST_LOCATION: "http://127.0.0.1:5000/api/locations/",
+  //   URL_PUT_LOCATION: "http://127.0.0.1:5000/api/locations/",
+  //   URL_DELETE_LOCATION: "http://127.0.0.1:5000/api/locations/",
+  // };
+
+  // const ipAdress =
+  //   process.env.NODE_ENV === "production"
+  //     ? "http://255.255.255"
+  //     : "http://localhost";
 
   return {
     getUrls: function () {
